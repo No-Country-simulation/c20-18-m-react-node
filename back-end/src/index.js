9cee20f0ed424306acb3b8536700faf2ce140136
Juan Ramirez <ciscojuan@gmail.com>
2024-08-29 09:06:12 -0500

    add morgan dependency

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const morgan = require('morgan')
+
 const routes = require("./routes");
 
 
 const app = express();
 const port = process.env.PORT;
-app.use(morgan('dev'))
 app.use(express.json());
 
 app.use('/api/v1', routes)
